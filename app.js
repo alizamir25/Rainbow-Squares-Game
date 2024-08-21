7031fcbce4d67c49b008cbc71033160c131d46ce
alizamir25 <ali0786zamir@gmail.com>
2024-08-22 00:19:45 +0100

    Patch

--- app.js
+++ app.js
@@ -10,16 +10,16 @@ for(let i = 0; i < squaresNumber; i++) {
 
     board.append(square)
 }
-function setColor(element) {
+function setColor(element){
     const color = getRandomColor()
     element.style.backgroundColor = color
     element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`
 }
-function removeColor(element) {
-    element.style.backgroundColor = '#1d1d1d'
+function removeColor(element){
+    element.style.backgroundColor = '#272626'
     element.style.boxShadow = `0 0 2px #000`
 }
-function getRandomColor() {
+function getRandomColor(){
    const index = Math.floor(Math.random() * colors.length)
    return colors[index]
 }
