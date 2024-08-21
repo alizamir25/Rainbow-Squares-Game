d6a35b3f4855a5ce4c48492491776d70d84b73b9
alizamir25 <ali0786zamir@gmail.com>
2024-08-22 00:19:11 +0100

    Patch

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const board = document.querySelector('#board')
 const colors = ['#e74c3c', '#f80000', '#8e44ad', '#fff', '#3498db', '#ffff00', '#e67e22', '#000ff', '#2ecc71']
 const squaresNumber = 500
-
 for(let i = 0; i < squaresNumber; i++) {
     const square = document.createElement('div')
     square.classList.add('square')
@@ -11,18 +10,15 @@ for(let i = 0; i < squaresNumber; i++) {
 
     board.append(square)
 }
-
 function setColor(element) {
     const color = getRandomColor()
     element.style.backgroundColor = color
     element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`
 }
-
 function removeColor(element) {
     element.style.backgroundColor = '#1d1d1d'
     element.style.boxShadow = `0 0 2px #000`
 }
-
 function getRandomColor() {
    const index = Math.floor(Math.random() * colors.length)
    return colors[index]
